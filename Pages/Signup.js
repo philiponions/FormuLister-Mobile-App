22a749d3a92669494ce8b3d7162020ab65fb12a8
philiponions <hueycoy9@gmail.com>
2023-04-22 13:44:25 -0600

    implementregistration frontend

--- Pages/Signup.js
+++ Pages/Signup.js
@@ -1,15 +1,43 @@
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import {  useFonts } from 'expo-font'
 import { useNavigation } from '@react-navigation/native'
+import axios from 'axios'
 
 
 const Signup = (props) => {  
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const navigation = useNavigation();
   
-  const goToSignup = () => {
-     navigation.navigate('Signup');
-     }
+  const handleSignup = () => {
+      
+    if (password === confirmPassword) {
+        if (password.length >= 6) {
+            // Api call good to go
+            axios.post("http://10.0.2.2:8000/user/register", {
+                username: username,
+                email: email,
+                password: password,        
+            }).then((response) => {
+                console.log(response.data);
+            }).catch((err) => {
+                console.log(err);
+            }).finally(() => {
+                // Go back to login regardless
+                navigation.navigate("Login");
+            })
+        } else {
+            // TODO: toast warnings
+            console.log("We recommend a password with 6 characters or more.")
+        }
+    } else {
+        // TODO: toast warnings
+        console.log("passwords must be the same");
+    }
+  }
 
   return (
     <View style={{flex: 1}}>
@@ -19,34 +47,34 @@ const Signup = (props) => {
         </View>
         
         <View style={styles.field}>
-            <TextInput style={styles.input} />
+            <TextInput style={styles.input} onChangeText={setEmail}/>
             <View style={styles.fieldType}>
                 <Text style={styles.fieldText}>Email</Text>
             </View>
         </View>
         
         <View style={styles.field}>
-            <TextInput style={styles.input} />
+            <TextInput style={styles.input} onChangeText={setUsername}/>
             <View style={styles.fieldType}>
                 <Text style={styles.fieldText}>Username</Text>
             </View>
         </View>
         
         <View style={styles.field}>
-            <TextInput style={styles.input} secureTextEntry={true}/>
+            <TextInput style={styles.input} onChangeText={setPassword} secureTextEntry={true}/>
             <View style={styles.fieldType}>
                 <Text style={styles.fieldText}>Password</Text>
             </View>
         </View>
 
         <View style={styles.field}>
-            <TextInput style={styles.input} secureTextEntry={true}/>
+            <TextInput style={styles.input} onChangeText={setConfirmPassword} secureTextEntry={true}/>
             <View style={styles.fieldType}>
                 <Text style={styles.fieldText}>Confirm Password</Text>
             </View>
         </View>
         
-        <TouchableOpacity style={styles.loginButton}>
+        <TouchableOpacity style={styles.loginButton} onPress={handleSignup}>
             <Text style={styles.loginText}>Sign up</Text>
         </TouchableOpacity>
       </View>
